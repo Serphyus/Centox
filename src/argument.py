54aff2882bb9b8bf06b37932c1d485be050b4f5b
Sigurd-Pettersen <73425444+Sigurd-Pettersen@users.noreply.github.com>
2022-03-16 19:57:49 +0100

    updated argument

--- src/argument.py
+++ src/argument.py
@@ -17,27 +17,12 @@ class Argument:
     def _hook(self):
         readline.insert_text(self._prefill)
         readline.redisplay()
-    
-    
-    def _reset_input(self, user_input: str) -> None:
-        line_size = os.get_terminal_size()[0]
-        full_prompt = len(user_input) + len(self._prompt)
-
-        total_reset = 1 + int(full_prompt / line_size)
-        Console.reset_lines(total_reset)
 
 
-    def update_argument(self) -> None:
-        while True:
-            readline.set_pre_input_hook(self._hook)
-            raw_user_input = input(self._prompt)
-            readline.set_pre_input_hook()
-            
-            self._reset_input(raw_user_input)
-
-            try:
-                user_input = raw_user_input.strip()
-                return user_input
-
-            except Exception:
-                continue
+    def prompt(self) -> None:
+        readline.set_pre_input_hook(self._hook)
+        raw_user_input = input(self._prompt)
+        readline.set_pre_input_hook()
+        
+        self._prefill = raw_user_input.strip()
+        return self._prefill
